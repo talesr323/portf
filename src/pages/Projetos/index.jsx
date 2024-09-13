080aea9dc03debdfff87f1b8c2b9f8e8e4feb1ef
Tales <talesr323@gmail.com>
2024-09-13 14:47:35 -0300

    Projects

--- src/pages/Projetos/index.jsx
+++ src/pages/Projetos/index.jsx
@@ -1,10 +1,39 @@
+import { useEffect, useState } from "react";
+import Card from "../../components/Card";
+import styles from './Projetos.module.css';
 
-function Projetos(){
-    return(
-        <>
-        <h2>Projetos</h2>
-        </>
-    )
+function Projetos() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        const buscarRepositorios = async () => {
+            const response = await fetch('https://api.github.com/users/talesr323/repos?page=18per_page=50');
+            const data = await response.json();
+            setRepositories(data);
+        };
+        buscarRepositorios();
+    }, []);
+
+    return (
+        <section className={styles.projetos}>
+            <h2>Projetos</h2>
+            {repositories.length > 0 ? (
+                <section className={styles.lista}>
+                    {repositories.map((repo) => (
+                        <Card
+                            key={repo.id}
+                            name={repo.name}
+                            description={repo.description}
+                            html_url={repo.html_url}
+                        />
+                    ))
+                    }
+                </section>
+            ) : (
+                <p>Carregando repositórios...</p>
+            )}
+        </section>
+    );
 }
 
-export default Projetos
+export default Projetos;
